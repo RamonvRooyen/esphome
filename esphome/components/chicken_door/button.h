7522abf688a7266aee478385334d4c0b496a6bfa
Ramon <ramon@eastbridge.eu>
2024-09-05 23:40:20 +0200

    all

--- esphome/components/chicken_door/button.h
+++ esphome/components/chicken_door/button.h
@@ -4,26 +4,28 @@
 #include "esphome/core/optional.h"
 
 #if defined(__arm__) || defined(ESP8266) || defined(ESP32)
-    #include <functional>
-    using fptr = std::function<void()>;
+#include <functional>
+using fptr = std::function<void()>;
 #else
-    typedef void (*fptr)();
+typedef void (*fptr)();
 #endif
 
 namespace esphome {
-class Button{
-public:
-    Button(int pin);
-    void onDown(const fptr& callback);
-    void onUp(const fptr& callback);
-    void loop();
-    bool isPressed();
-    bool debug = false;
+namespace chicken_door {
+class Button {
+ public:
+  Button(int pin);
+  void onDown(const fptr &callback);
+  void onUp(const fptr &callback);
+  void loop();
+  bool isPressed();
+  bool debug = false;
 
-private:
-    bool buttonPressed = false;
-    int pin;
-    esphome::optional<fptr> downCallback;
-    esphome::optional<fptr> upCallback;
+ private:
+  bool buttonPressed = false;
+  int pin;
+  esphome::optional<fptr> downCallback;
+  esphome::optional<fptr> upCallback;
 };
-}
+}  // namespace chicken_door
+}  // namespace esphome
--- esphome/components/chicken_door/config.h
+++ esphome/components/chicken_door/config.h
@@ -1,4 +1,8 @@
+namespace esphome {
+namespace chicken_door {
 #define DEBUG_MODE true
 #define DEBUG_BUTTON false
 #define DEBUG_MOTOR true
 #define TELEMETRY_ENABLED false
+}
+}
